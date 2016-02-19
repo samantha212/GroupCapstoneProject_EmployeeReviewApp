ced86673ca5a78e1876ff0163de5b0274af5ac25
Samantha Musselman <samantha.musselman212@gmail.com>
2016-02-19 15:12:22 -0600

    Add goals view.

--- scripts/main.js
+++ scripts/main.js
@@ -46,4 +46,10 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 
 app.controller('HomeController', ['$scope', '$http', function($scope, $http){
   //DO STUFF
+  //$scope.empOneTwo = true;
+  //$scope.empThreePlus = true;
+  //$scope.empFivePlus = true;
+  //$scope.leaderThreePlus = true;
+  //$scope.leaderThreeFourFive = true;
+  //$scope.leaderSixPlus = true;
 }]);
