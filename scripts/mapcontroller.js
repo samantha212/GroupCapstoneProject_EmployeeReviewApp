94ef177d65ec11d50e3872ed84db1e93d92436de
Samantha Musselman <samantha.musselman212@gmail.com>
2016-02-22 14:48:54 -0600

    Sketches out map view and required mapcontroller variables.

--- scripts/mapcontroller.js
+++ scripts/mapcontroller.js
@@ -1,3 +1,30 @@
 app.controller('MapController', ['$scope', '$http', function($scope, $http){
-  //DO STUFF
+    $scope.typeField = true;
+    $scope.typeSalon = true;
+
+    $scope.financesComplete = true;
+    $scope.goalOneComplete = true;
+    $scope.goalTwoComplete = true;
+    $scope.goalThreeComplete = false;
+    $scope.goalFourComplete = false;
+    $scope.goalFiveComplete = false;
+
+    $scope.hComplete = true;
+    $scope.aComplete = true;
+    $scope.iComplete = false;
+    $scope.rComplete = false;
+    $scope.hairRatingComplete = false;
+
+    $scope.strengthsComplete = true;
+    $scope.developmentComplete = false;
+    $scope.finalRatingComplete = false;
+
+    $scope.empOneTwo = true;
+    $scope.empThreePlus = true;
+    $scope.empFivePlus = true;
+    $scope.empSix = true;
+    $scope.leaderThreePlus = true;
+    $scope.leaderThreeFourFive = true;
+    $scope.leaderSixPlus = true;
 }]);
+
