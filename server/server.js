c3a8b7242fdc0c6924f719d7eb207978ba6c758e
Altamir Coelho <alt.coelho@gmail.com>
2016-02-22 11:18:44 -0600

    sass, angular routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ app.get('/', function(request,response){
   response.sendFile(path.join(__dirname, '/public/views/index.html'));
 });
 
+// temporary "Catch All" for reloading angular routes
+app.get('/*', function(request, response){
+    response.sendFile(path.join(__dirname, '/public/views/index.html'));
+});
+
 var server = app.listen(3000, function(){
     var port = server.address().port;
     console.log("Listening on port: ", port);
