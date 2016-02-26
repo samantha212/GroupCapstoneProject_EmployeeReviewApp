a94a75f1a73b8978fca5ae5266efe00c82769d1c
Charlie <charliedevelopmentaccess@gmail.com>
2016-02-26 15:11:43 -0600

    Updated server file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var index = require('./routes/index');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, '/public')));
-app.get('/', function(request,response){
-  response.sendFile(path.join(__dirname, '/public/views/index.html'));
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// temporary "Catch All" for reloading angular routes
-app.get('/*', function(request, response){
-    response.sendFile(path.join(__dirname, '/public/views/index.html'));
-});
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/regiscorp';
 
-var server = app.listen(3000, function(){
-    var port = server.address().port;
-    console.log("Listening on port: ", port);
+app.use('/', index);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+  console.log('running on port: ', app.get('port'));
 });
