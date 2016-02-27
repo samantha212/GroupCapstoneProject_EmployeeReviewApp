1df332819fe887b189e76fee11da3c74c321977e
Samantha Musselman <samantha.musselman212@gmail.com>
2016-02-27 13:51:05 -0600

    Update getMapArray functionality

--- scripts/main.js
+++ scripts/main.js
@@ -70,15 +70,7 @@ app.controller('HomeController', ['$scope', '$http', 'ReviewService', function($
   //$scope.loadHomePageInfo = ReviewService.loadHomePageInfo;
 
 
-      //***** We need the back end team to update this query to take in the employee's RegisId instead of the plain id. *****
-      //We are going to leave this for now, but will need to come back and update the data sent in the POST call.
-      //In an object send down the employeeId of the employee you would like to get the current map view information of.
-      //Id in the employee information object is the same Id as EmployeeId in the other objects, match the two to know who you are dealing with
-      //The 2 in this object is the employee review with the id of 2, once you start getting the client logic running you will want this objects id value to be dynamic to which review you click on, but for now we decided to hard code the employee with the id of 2
-      var giveMeThisEmployeesMapView = {Id: 2};
-$http.post('/getmapview', giveMeThisEmployeesMapView).then(function(response){
-  console.log(response, 'map view logic');
-});
+
 });
 
 
@@ -94,14 +86,12 @@ $http.post('/getmapview', giveMeThisEmployeesMapView).then(function(response){
 
 
 
-
 app.factory('ReviewService', ['$http', function($http) {
 
   var thisUser = {};
   var myReview = {};
   var teamReviews =[];
   var currentReview = {};
-  var mapArray = [];
 
   //default all these vars to false\\
   var leader = false;
@@ -152,7 +142,8 @@ app.factory('ReviewService', ['$http', function($http) {
   };
 
   return {
-    loadHomePageInfo: loadHomePageInfo
+    loadHomePageInfo: loadHomePageInfo,
+    thisUser: thisUser
   };
 
 
--- scripts/mapcontroller.js
+++ scripts/mapcontroller.js
@@ -1,4 +1,8 @@
-app.controller('MapController', ['$scope', '$http', function($scope, $http){
+app.controller('MapController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
+    var thisUser = ReviewService.thisUser;
+    var mapArray = [];
+    $scope.getMapArray = giveMeThisEmployeesMapView;
+
     $scope.typeField = true;
     $scope.typeSalon = true;
 
@@ -19,4 +23,15 @@ app.controller('MapController', ['$scope', '$http', function($scope, $http){
     $scope.developmentComplete = false;
     $scope.finalRatingComplete = false;
 
+    //***** We need the back end team to update this query to take in the employee's RegisId instead of the plain id. *****
+    //We are going to leave this for now, but will need to come back and update the data sent in the POST call.
+    //In an object send down the employeeId of the employee you would like to get the current map view information of.
+    //Id in the employee information object is the same Id as EmployeeId in the other objects, match the two to know who you are dealing with
+    //The 2 in this object is the employee review with the id of 2, once you start getting the client logic running you will want this objects id value to be dynamic to which review you click on, but for now we decided to hard code the employee with the id of 2
+    var giveMeThisEmployeesMapView = {Id: 2};
+    $http.post('/getmapview', giveMeThisEmployeesMapView).then(function(response){
+        console.log(response, 'map view logic');
+    });
+
+// ***** We need to come back and connect each of the true/false values above to the response object.
 }]);
