eee297fcdee48aaaf3528bfc9ceeef2a24989876
Charlie <charliedevelopmentaccess@gmail.com>
2016-02-29 13:12:15 -0600

    Created put function

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,16 @@ var path = require('path');
 var token = require('../routes/tokencall');
 var employeeReview = require('../routes/entireemployeereview.js');
 var mapView = require('../routes/mapview.js');
+var updateData = require('../routes/putsection.js');
 
 router.use('/', express.static(path.join(__dirname, '../public')));
 router.get('/', function(request, response){
   response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
-//link the token module
 router.use('/token', token);
 router.use('/employeeData', employeeReview);
 router.use('/getmapview', mapView)
+router.use('/updateData', updateData);
 
 module.exports = router;
---
+++ server/routes/putsection.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/regiscorp';
+
+var empId = [];
+router.post('/', function(request, response){
+  var employeeUpdate = {regisId: request.body.regisId, SectionId: request.body.SectionId, SubsectionId: request.body.SubsectionId, data: request.body.data};
+
+  pg.connect(connectionString, function(err, client, done){
+    var findEmpId = client.query('SELECT "Id" FROM "employeeData" WHERE "RegisId" = $1', [employeeUpdate.regisId]);
+
+    findEmpId.on('row', function(row){
+      empId.push(row);
+    });
+
+    findEmpId.on('end', function(){
+      var query = client.query('UPDATE "Subsection" SET "Goal" = $1, "EmployeeGoalRating" = $2, "EmployeeResponse" = $3, "LeaderGoalRating" = $4, "LeaderResponse" = $5, "isCompleted" = $6, "isLeaderCompleted" = $7, "EmployeeHairRating" = $8, "LeaderHairRating" = $9, "EmployeeFinalRating" = $10, "LeaderFinalRating" = $11, "SS_Actual" = $12, "SS_Target" = $13, "RS_Actual" = $14, "RS_Target" = $15, "TS_Actual" = $16, "TS_Target" = $17, "TS_Rating" = $18, "C_Actual" = $19, "C_Target" = $20, "C_Rating" = $21, "SGC_Actual" = $22, "SGC_Target" = $23, "SGC_Rating" = $24, "OverallRating" = $25 WHERE "EmployeeId" = $26 AND "SectionId" = $27 AND "SubsectionId" = $28', [employeeUpdate.data.Goal, employeeUpdate.data.EmployeeGoalRating, employeeUpdate.data.EmployeeResponse, employeeUpdate.data.LeaderGoalRating, employeeUpdate.data.LeaderResponse, employeeUpdate.data.isCompleted, employeeUpdate.data.isLeaderCompleted, employeeUpdate.data.EmployeeHairRating, employeeUpdate.data.LeaderHairRating, employeeUpdate.data.EmployeeFinalRating, employeeUpdate.data.LeaderFinalRating, employeeUpdate.data.SS_Actual, employeeUpdate.data.SS_Target, employeeUpdate.data.RS_Actual, employeeUpdate.data.RS_Target, employeeUpdate.data.TS_Actual, employeeUpdate.data.TS_Target, employeeUpdate.data.TS_Rating, employeeUpdate.data.C_Actual, employeeUpdate.data.C_Target, employeeUpdate.data.C_Rating, employeeUpdate.data.SGC_Actual, employeeUpdate.data.SGC_Target, employeeUpdate.data.SGC_Rating, employeeUpdate.data.OverallRating, empId[0].Id, employeeUpdate.SectionId, employeeUpdate.SubsectionId]);
+
+      response.sendStatus(200);
+      client.end();
+    });
+    if(err){response.send('error on server')};
+  });
+});
+
+module.exports = router;
