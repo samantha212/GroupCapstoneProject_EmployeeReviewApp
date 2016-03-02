7837ce5b900b42cb5fd73eda24c5923abe2eaac9
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-02 15:37:58 -0600

    Update ng var names for strengths/devs.

--- scripts/strengthsdevscontroller.js
+++ scripts/strengthsdevscontroller.js
@@ -1,4 +1,10 @@
 app.controller('StrengthsDevsController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
     //$scope.strengthDevData = ReviewService.reviews.currentReview[1][ReviewService.subsections.currentStrengthDev];
-    $scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
+    //$scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
+
+    $scope.currentReviewEmpInfo = ReviewService.currentReview.empInfo;
+    $scope.strengthDevData = ReviewService.currentReview.subsections;
+
+    $scope.subsections = ReviewService.subsections;
+
 }]);
