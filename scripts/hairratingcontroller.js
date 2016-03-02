f9db56d282422efb376f965e34efac8017949dbe
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-02 15:27:36 -0600

    Updates ng vars for hair rating page.

--- scripts/hairratingcontroller.js
+++ scripts/hairratingcontroller.js
@@ -1,4 +1,8 @@
 app.controller('HairRatingController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
-  $scope.hairRatingData = ReviewService.reviews.currentReview[1][10];
-  $scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
+  $scope.currentReviewEmpInfo = ReviewService.currentReview.empInfo;
+  $scope.hairRatingData = ReviewService.currentReview.subsections;
+  $scope.subsections = ReviewService.subsections;
+
+  //$scope.hairRatingData = ReviewService.reviews.currentReview[1][10];
+  //$scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
 }]);
