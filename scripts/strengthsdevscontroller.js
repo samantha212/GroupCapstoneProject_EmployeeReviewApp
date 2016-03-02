a2c2682d1113944a89bf99f2eda2fd296505e9fc
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-02 11:09:13 -0600

    Update ng vars for strength/dev view.

--- scripts/strengthsdevscontroller.js
+++ scripts/strengthsdevscontroller.js
@@ -1,4 +1,4 @@
 app.controller('StrengthsDevsController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
-    $scope.strengthDevData = ReviewService.reviews.currentReview[1][ReviewService.subsections.currentStrengthDev];
+    //$scope.strengthDevData = ReviewService.reviews.currentReview[1][ReviewService.subsections.currentStrengthDev];
     $scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
 }]);
