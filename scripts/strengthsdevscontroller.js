d83da62444a663baf3dbf344d058b5e5e7e1b1f5
Charlie <charliedevelopmentaccess@gmail.com>
2016-03-03 16:53:25 -0600

    All put calls built, all form client side validation finished

--- scripts/strengthsdevscontroller.js
+++ scripts/strengthsdevscontroller.js
@@ -1,14 +1,37 @@
 app.controller('StrengthsDevsController', ['$scope', '$http', '$location', 'ReviewService', function($scope, $http, $location, ReviewService){
     $scope.currentReviewEmpInfo = ReviewService.currentReview.empInfo;
     $scope.strengthDevData = ReviewService.currentReview.subsections;
+    var role = ReviewService.role;
 
     $scope.subsections = ReviewService.subsections;
 
     $scope.putCompleteAndGoNextStrengthDev = function() {
-        console.log('putCompleteAndGoNextStrengthDev function hit');
-        //put that subsection to the DB;
-        //Mark subsection as complete.
-        $scope.goNextStrengthDev();
+      var leaderIsComp = false;
+      var empIsComp = false;
+
+      if(role.leader == true){
+        leaderIsComp = true;
+      }else{
+        empIsComp = true;
+      }
+      var contentToSend = {regisId: $scope.currentReviewEmpInfo.RegisId, SectionId: 5, SubsectionId: ReviewService.subsections.currentStrengthDev-10, data: {EmployeeResponse: $scope.strengthDevData[ReviewService.subsections.currentStrengthDev].EmployeeResponse, LeaderResponse: $scope.strengthDevData[ReviewService.subsections.currentStrengthDev].LeaderResponse, isCompleted: empIsComp, isLeaderCompleted: leaderIsComp}};
+
+      $http.post('/updateData', contentToSend).then(function(response){
+        if(response.status == 200){
+          $scope.goNextStrengthDev();
+        }
+      });
+    };
+
+    $scope.putAndGoMap = function(){
+      var contentToSend = {regisId: $scope.currentReviewEmpInfo.RegisId, SectionId: 5, SubsectionId: ReviewService.subsections.currentStrengthDev-10, data: {EmployeeResponse: $scope.strengthDevData[ReviewService.subsections.currentStrengthDev].EmployeeResponse, LeaderResponse: $scope.strengthDevData[ReviewService.subsections.currentStrengthDev].LeaderResponse}};
+
+      $http.post('/updateData', contentToSend).then(function(response){
+        if(response.status == 200){
+          ReviewService.getReview($scope.currentReviewEmpInfo.RegisId);
+          $location.path('/map');
+        }
+      });
     };
 
     $scope.goNextStrengthDev = function() {
