8bd46f14fa88a8d44fea5c505f0f6d21d32ceb25
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-03 11:59:59 -0600

    Adds navigation function to Strength/Dev views.

--- scripts/strengthsdevscontroller.js
+++ scripts/strengthsdevscontroller.js
@@ -1,10 +1,26 @@
-app.controller('StrengthsDevsController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
-    //$scope.strengthDevData = ReviewService.reviews.currentReview[1][ReviewService.subsections.currentStrengthDev];
-    //$scope.currentReviewEmpInfo = ReviewService.reviews.currentReview[0][0];
-
+app.controller('StrengthsDevsController', ['$scope', '$http', '$location', 'ReviewService', function($scope, $http, $location, ReviewService){
     $scope.currentReviewEmpInfo = ReviewService.currentReview.empInfo;
     $scope.strengthDevData = ReviewService.currentReview.subsections;
 
     $scope.subsections = ReviewService.subsections;
+
+    $scope.putCompleteAndGoNextStrengthDev = function() {
+        console.log('putCompleteAndGoNextStrengthDev function hit');
+        //put that subsection to the DB;
+        //Mark subsection as complete.
+        $scope.goNextStrengthDev();
+    };
+
+    $scope.goNextStrengthDev = function() {
+        console.log('goNextStrengthDev function hit');
+
+        if (ReviewService.subsections.currentStrengthDev < 12) {
+            ReviewService.subsections.currentStrengthDev += 1;
+            console.log('current strength/dev:', ReviewService.subsections.currentStrengthDev);
+        } else {
+            $location.path('/final-rating');
+        }
+    };
+
 
 }]);
