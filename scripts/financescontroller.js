93a5ad6e8a78fdb08375f056b60b6f35250b0f23
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-03 14:58:48 -0600

    Updates nav functionality for finances page buttons.

--- scripts/financescontroller.js
+++ scripts/financescontroller.js
@@ -1,11 +1,25 @@
-app.controller('FinancesController', ['$scope', '$http', 'ReviewService', function($scope, $http, ReviewService){
+app.controller('FinancesController', ['$scope', '$http', '$location', 'ReviewService', function($scope, $http, $location, ReviewService){
     $scope.currentReview = ReviewService.currentReview;
     $scope.financeData = ReviewService.currentReview.subsections[0];
 
     $scope.makeVariance = function(firstValue, secondValue){
       return firstValue - secondValue;
-    }
+    };
      $scope.makePercentage = function(firstValue, secondValue){
       return ((firstValue / secondValue) * 100).toFixed(1);
-    }
+    };
+
+    $scope.putCompleteAndGoGoals = function() {
+        console.log('putCompleteAndGoGoals function hit');
+        //put that subsection to the DB;
+        $scope.goGoals();
+    };
+
+    $scope.goGoals = function() {
+        console.log('goGoals function hit');
+        ReviewService.subsections.currentGoal = 1;
+        $location.path('/goals');
+    };
+
+
 }]);
